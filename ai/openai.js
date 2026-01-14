7341dafdd41d920bf565a3b31eec442d0a50ede8
Dmitry Sharabin <dmitrysharabin@gmail.com>
2026-01-14 17:01:50 +0100

    [ai] [OpenAI] Basic setup

---
+++ ai/openai.js
@@ -0,0 +1,274 @@
+import fs from "node:fs";
+import { loadEnvFile } from "node:process";
+import OpenAI from "openai";
+import { codebookSchema } from "./util.js";
+
+loadEnvFile(".env");
+
+const client = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
+// File operations: https://platform.openai.com/docs/api-reference/files?lang=node.js
+
+// It's an example of how to use the API to generate content.
+async function main () {
+	// See https://platform.openai.com/docs/guides/structured-outputs?example=structured-data#streaming
+	const stream = client.responses
+		.stream({
+			model: "gpt-5-nano",
+			input: [
+				{
+					type: "message",
+					role: "system",
+					content:
+						"You are a helpful assistant that generates a list of famous physicists.",
+				},
+				{
+					type: "message",
+					role: "user",
+					content: "Provide a list of 3 famous physicists.",
+				},
+			],
+			text: {
+				format: {
+					name: "famous_physicists",
+					type: "json_schema",
+					strict: true,
+					schema: {
+						title: "Famous Physicist",
+						type: "object",
+						properties: {
+							physicists: {
+								type: "array",
+								items: {
+									type: "object",
+									properties: {
+										first_name: {
+											type: "string",
+										},
+										// Can't have optional properties in a JSON schema (WTF OpenAI?)
+										// middle_name: {
+										// 	type: "string",
+										// },
+										last_name: {
+											type: "string",
+										},
+									},
+									required: ["first_name", "last_name"],
+									additionalProperties: false,
+								},
+							},
+						},
+						required: ["physicists"],
+						additionalProperties: false,
+					},
+				},
+			},
+		})
+		.on("response.refusal.delta", event => {
+			console.log(event.delta);
+		})
+		.on("response.output_text.delta", event => {
+			console.log(event.delta);
+			// Example output:
+			// {"
+			// phys
+			// ic
+			// ists
+			// ":[
+			// {"
+			// first
+			// _name
+			// ":"
+			// Albert
+			// ","
+			// last
+			// _name
+			// ":"
+			// Ein
+			// stein
+			// "},{"
+			// first
+			// _name
+			// ":"
+			// Marie
+			// ","
+			// last
+			// _name
+			// ":"
+			// Cur
+			// ie
+			// "},{"
+			// first
+			// _name
+			// ":"
+			// N
+			// iels
+			// ","
+			// last
+			// _name
+			// ":"
+			// Bo
+			// hr
+			// "}
+			// ]}
+		})
+		.on("response.output_text.done", () => {
+			console.log("\n");
+		})
+		.on("response.error", event => {
+			console.error(event.error);
+		});
+
+	let result = await stream.finalResponse();
+
+	console.log(result);
+	// Example output:
+	// {
+	//   id: 'resp_05206ecbace67e31006967affe1b94819d99098a6a63a251e4',
+	//   object: 'response',
+	//   created_at: 1768402942,
+	//   status: 'completed',
+	//   background: false,
+	//   completed_at: 1768402948,
+	//   error: null,
+	//   frequency_penalty: 0,
+	//   incomplete_details: null,
+	//   instructions: null,
+	//   max_output_tokens: null,
+	//   max_tool_calls: null,
+	//   model: 'gpt-5-nano-2025-08-07',
+	//   output: [
+	//     {
+	//       id: 'rs_05206ecbace67e31006967affeab4c819d99c28a9ca6dc2d3c',
+	//       type: 'reasoning',
+	//       summary: []
+	//     },
+	//     {
+	//       id: 'msg_05206ecbace67e31006967b0044c14819d9394bb35a7295e5d',
+	//       type: 'message',
+	//       status: 'completed',
+	//       content: [Array],
+	//       role: 'assistant'
+	//     }
+	//   ],
+	//   parallel_tool_calls: true,
+	//   presence_penalty: 0,
+	//   previous_response_id: null,
+	//   prompt_cache_key: null,
+	//   prompt_cache_retention: null,
+	//   reasoning: { effort: 'medium', summary: null },
+	//   safety_identifier: null,
+	//   service_tier: 'default',
+	//   store: true,
+	//   temperature: 1,
+	//   text: {
+	//     format: {
+	//       type: 'json_schema',
+	//       description: null,
+	//       name: 'famous_physicists',
+	//       schema: [Object],
+	//       strict: true
+	//     },
+	//     verbosity: 'medium'
+	//   },
+	//   tool_choice: 'auto',
+	//   tools: [],
+	//   top_logprobs: 0,
+	//   top_p: 1,
+	//   truncation: 'disabled',
+	//   usage: {
+	//     input_tokens: 92,
+	//     input_tokens_details: { cached_tokens: 0 },
+	//     output_tokens: 630,
+	//     output_tokens_details: { reasoning_tokens: 576 },
+	//     total_tokens: 722
+	//   },
+	//   user: null,
+	//   metadata: {},
+	//   output_parsed: null
+	// }
+}
+
+// await main();
+
+async function developCodebook (filename = "files/starting_codes.json") {
+	const model = "gpt-5.2-pro";
+
+	const systemPrompt = `
+Imagine you are a senior qualitative data researcher with a strong background in front-end web development (you know the entirety of MDN by heart),
+web standards, browser ecosystems, and the web platform.
+`;
+
+	const codebookPrompt = `...`;
+
+	const file = await client.files.create({
+		file: fs.createReadStream(filename),
+		purpose: "user_data",
+	});
+
+	const stream = client.responses
+		.stream({
+			model,
+			reasoning: {
+				effort: "medium", // See https://platform.openai.com/docs/guides/reasoning
+			},
+			input: [
+				{
+					type: "message",
+					role: "system",
+					content: systemPrompt,
+				},
+				{
+					type: "message",
+					role: "user",
+					content: [
+						{
+							type: "text",
+							text: codebookPrompt,
+						},
+						{
+							// https://platform.openai.com/docs/guides/pdf-files#page-top
+							type: "input_file",
+							file_id: file.id,
+						},
+					],
+				},
+			],
+			text: {
+				verbosity: "low", // See https://platform.openai.com/docs/guides/latest-model#verbosity
+				format: {
+					name: "codebook",
+					type: "json_schema",
+					strict: true,
+					schema: {
+						title: "Codebook",
+						type: "object",
+						properties: {
+							codes: {
+								...codebookSchema.schema,
+							},
+						},
+						required: ["codes"],
+						additionalProperties: false,
+					},
+				},
+			},
+		})
+		.on("response.refusal.delta", event => {
+			// Handle the refusal
+		})
+		.on("response.output_text.delta", event => {
+			// Stream the response
+		})
+		.on("response.output_text.done", () => {
+			// We are done
+		})
+		.on("response.error", event => {
+			// Handle the error
+		});
+
+	let result = await stream.finalResponse();
+	// Do something with the result, if needed
+}
