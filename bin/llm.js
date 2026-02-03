726e3f90a834f4248179476ab6d3bade59d7d775
Lea Verou <lea@verou.me>
2026-02-02 19:38:07 -0500

    More robust argument parsing, make task the positional arg
    Rationale:
    - task is more fundamental than question id
    - eventually we may want to allow tasks w/o question ids, that will do all questions

--- bin/llm.js
+++ bin/llm.js
@@ -1,51 +1,101 @@
+#!/usr/bin/env node
 /**
  * Main entry point.
  * Dynamically loads the specified LLM adapter and runs the shared LLM orchestrator.
  * @param {string} questionId - The ID of the question to be coded.
  * @param {string} [llm=gemini] - The LLM to use (e.g., "gemini", "openai", "claude").
  * @param {string} [model] - The specific model for the LLM to use (e.g., "gemini-3-pro-preview").
- * @param {string} [step=answers] - The step to perform ("answers" or "codebook").
+ * @param {string} [task=answers] - The task to perform ("answers" or "codebook").
  * @param {boolean} [fresh] - Whether to force fresh upload of files (yes, if present).
  * @example
  * node index.js browser_apis --llm openai --fresh
  */
 
-import { argv, loadEnvFile } from "node:process";
+import { loadEnvFile } from "node:process";
 import { existsSync } from "node:fs";
-import Question from "./question.js";
+import { readDirectorySync } from "../src/util.js";
+import { readArgs } from "./util.js";
+import Question from "../src/question.js";
 
 if (existsSync(".env")) {
 	loadEnvFile(".env");
 }
 
-const questionId = argv[2];
+const availableOptions = {
+	llmId: {
+		long: "llm",
+		default: "gemini",
+	},
+	model: {
+		default: undefined,
+	},
+	task: {
+		default: "answers",
+	},
+	fresh: {
+		default: false,
+		short: "f",
+	},
+	questionId: {
+		long: "question",
+		short: "q",
+	},
+};
+
+const args = readArgs(process.argv.slice(2), availableOptions);
+
+const { questionId, llmId, model, fresh } = args;
+const taskId = args._[0] ?? "answers";
+
 if (!questionId) {
-	console.error("Please provide a question ID as the first argument. For example, browser_apis.");
+	console.error(
+		"Please provide a question ID via the -q/--question flag. Available ids: ",
+		Question.ids.join(", "),
+	);
+	process.exit(1);
+}
+else if (!Question.ids.includes(questionId)) {
+	console.error(
+		`The question ID “${questionId}” is not valid. Available ids: `,
+		Question.ids.join(", "),
+	);
 	process.exit(1);
 }
 
 const question = Question.fromId(questionId);
 
-let llm = argv.includes("--llm") ? argv[argv.indexOf("--llm") + 1] : "gemini";
-let model = argv.includes("--model") ? argv[argv.indexOf("--model") + 1] : undefined;
-let step = argv.includes("--step") ? argv[argv.indexOf("--step") + 1] : "answers";
-let fresh = argv.includes("--fresh") || undefined;
-
-let llmPath = `../llms/${llm}.js`;
+let llmPath = `../llms/${llmId}.js`;
 let module;
 try {
 	module = await import(llmPath);
 }
 catch (e) {
-	console.error(`Failed to load the LLM adapter from “${llmPath}”.`);
+	console.error(`Failed to load the LLM module from “${llmPath}”.`);
 	console.error(e);
 	process.exit(1);
 }
 
-const Adapter = module.default ?? module;
-const runner = new Adapter({ fresh, model });
+const LLM = module.default ?? module;
+const runner = new LLM({ fresh, model });
 
-let task = await import(`../tasks/${step}/index.js`).then(module => module.default);
+let task;
+let taskPath = `../tasks/${taskId}/index.js`;
+try {
+	task = await import(taskPath).then(module => module.default);
+}
+catch (e) {
+	if (!existsSync(taskPath)) {
+		console.error(
+			`The task ID “${taskId}” is not valid. Available ids: `,
+			readDirectorySync("../tasks", { type: "directory" }).join(", "),
+		);
+		process.exit(1);
+	}
+
+	console.error(`Failed to load the task module for “${taskId}” from “${taskPath}”.`);
+	console.error(e);
+	process.exit(1);
+}
 
 await runner.runTask(task, question);
 process.exit();
---
+++ bin/util.js
@@ -0,0 +1,34 @@
+import minimist from "minimist";
+
+export function readArgs (argv = process.argv.slice(2), availableOptions) {
+	let args = minimist(argv);
+	let ret = {
+		_: args._,
+	};
+
+	for (let key in availableOptions) {
+		let option = availableOptions[key];
+		let long = option.long ?? key;
+
+		if (long in args) {
+			ret[key] = args[long];
+		}
+		else if (option.short in args) {
+			ret[key] = args[option.short];
+		}
+
+		if (ret[key] !== undefined) {
+			if (option.parse) {
+				ret[key] = option.parse(ret[key]);
+			}
+
+			if (option.validate && !option.validate(ret[key])) {
+				delete ret[key];
+			}
+		}
+
+		ret[key] ??= option.default;
+	}
+
+	return ret;
+}
