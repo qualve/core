97e41e7cdf4c8675a45d6cdd6dce5ab7907db94a
Lea Verou <lea@verou.me>
2026-02-06 19:31:02 -0500

    [graphql] `Task` class (#32)
    - Runners become subclasses instead of ad hoc modules that need to follow a certain structure. This way they can also selectively override things.
    - Better API for several things, e.g. getting ids, processing task objects, etc
    - We now support compound tasks that can be run either sequentially or in parallel! Question ids and other parameters are passed down for any tasks that may need them. LLM tasks automatically disable parallelization for both themselves and all their ancestors.
    - And multiple question ids become parallel subtasks, and can be executed in any order
    - No weird passing of `confirm` to the programmatic API — it doesn't need it. When we use this programmatically, we are specifying either one or multiple question ids.
    - Support for custom loggers (e.g. for debug logs)
    - Proper indentation for subtasks

--- graphql/src/types/graphql.js
+++ graphql/src/types/graphql.js
@@ -1,9 +1,47 @@
 import { writeJSONSync } from "../util.js";
 import survey from "../../survey.js";
-
-export const id = "graphql";
+import Task from "./task.js";
 
 const ENDPOINT = "https://api.devographics.com/graphql";
+
+export default class GraphQLTask extends Task {
+	async runTask (question) {
+		let query = this.fields;
+
+		if (this.scope === "survey" || this.scope === "question") {
+			if (this.scope === "question") {
+				query = { [question.section]: { [question.id]: query } };
+			}
+
+			query = { surveys: { [survey.name]: { [survey.id]: query } } };
+		}
+
+		query = stringifyQuery(query, "query");
+
+		let result = await runQuery(query);
+
+		if (result) {
+			result = result?.data;
+
+			if (this.scope === "survey" || this.scope === "question") {
+				result = result.surveys[survey.name][survey.id];
+
+				if (this.scope === "question") {
+					result = result[question.section][question.id];
+				}
+			}
+
+			result = this.handleResult?.(result, question) ?? result;
+
+			if (this.output) {
+				var outputPath = `data${this.scope === "question" ? "/" + question.id : ""}/${this.output}`;
+				var size = writeJSONSync(outputPath, result)?.length;
+			}
+		}
+
+		return { result, query, outputPath, size };
+	}
+}
 
 export function stringifyQuery (value, key) {
 	if (key) {
@@ -54,41 +92,4 @@ export async function runQuery (query, endpoint = ENDPOINT) {
 	}
 
 	return json;
-}
-
-export async function runTask (task, question) {
-	let query = task.fields;
-
-	if (task.scope === "survey" || task.scope === "question") {
-		if (task.scope === "question") {
-			query = { [question.section]: { [question.id]: query } };
-		}
-
-		query = { surveys: { [survey.name]: { [survey.id]: query } } };
-	}
-
-	query = stringifyQuery(query, "query");
-
-	let result = await runQuery(query);
-
-	if (result) {
-		result = result?.data;
-
-		if (task.scope === "survey" || task.scope === "question") {
-			result = result.surveys[survey.name][survey.id];
-
-			if (task.scope === "question") {
-				result = result[question.section][question.id];
-			}
-		}
-
-		result = task.handleResult?.(result, question) ?? result;
-
-		if (task.output) {
-			var outputPath = `data${task.scope === "question" ? "/" + question.id : ""}/${task.output}`;
-			var size = writeJSONSync(outputPath, result)?.length;
-		}
-	}
-
-	return { result, query, outputPath, size };
 }
