af1994c94ead304213e3fad10293be94e899fa08
Lea Verou <lea@verou.me>
2026-03-23 19:42:50 -0400

    Subset files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import Task from "./task.js";
+export { default as default } from "./task.js";
+export { default as Task } from "./task.js";
+export { default as DataTask } from "./types/data.js";
+export { default as Entity } from "./entity.js";
 
-export * from "./types/index.js";
-export * from "./llms/index.js";
-
-export default Task;
+export * as util from "./util.js";
