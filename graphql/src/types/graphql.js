10b34ec78d1add02cdede3f8a30bd461bd966903
Lea Verou <lea@verou.me>
2026-03-24 01:04:03 -0400

    [graphql] Move GraphQL utils to util

--- graphql/src/types/graphql.js
+++ graphql/src/types/graphql.js
@@ -1,5 +1,6 @@
-import { writeJSONSync } from "../util.js";
-import Task from "../task.js";
+import Task from "qualve/task";
+import { writeJSONSync } from "qualve/util";
+import { stringifyQuery, runQuery } from "./util.js";
 
 export default class GraphQLTask extends Task {
 	static type = "graphql";
@@ -42,57 +43,6 @@ export default class GraphQLTask extends Task {
 
 		return { result, query, outputPath, size };
 	}
-}
-
-export function stringifyQuery (value, key) {
-	if (key) {
-		if (typeof value === "object") {
-			return `${key} { ${stringifyQuery(value)} }`;
-		}
-		else if (typeof value === "string") {
-			return `${key} { ${value} }`;
-		}
-
-		return key;
-	}
-
-	if (Array.isArray(value)) {
-		return value.map(item => stringifyQuery(item)).join(" ");
-	}
-	if (typeof value === "object") {
-		return Object.entries(value)
-			.map(([key, value]) => stringifyQuery(value, key))
-			.join(" ");
-	}
-
-	return value;
-}
-
-async function runQuery (query, endpoint) {
-	const response = await fetch(endpoint, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ query }),
-	});
-
-	try {
-		var json = await response.json();
-	}
-	catch (e) {
-		var text = await response.text();
-	}
-
-	if (!response.ok) {
-		let errors = json?.errors ?? [{ message: text }];
-		for (const error of errors) {
-			console.error(`GraphQL error: ${error.message}. Query: ${query}`);
-		}
-		return null;
-	}
-
-	return json;
 }
 
 Task.register(GraphQLTask);
